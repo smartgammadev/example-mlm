a48f7ea06d3a02805113141f7be93a1d3cd8bd6c
Stas81 <stas-81@mail.ru>
2014-10-30 11:56:51 +0200

    Deploy

---
+++ app/config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "4success.smart-gamma.com"
+set :domain,      "4success.smart-gamma.com"
+set :user,        "4success"
+set :deploy_to,   "/home/4success/4success"
+set :app_path,    "app"
+
+set :repository,  "git@bitbucket.org:stas81/4success.git"
+set :scm,         :git
+set :branch,      "master"
+
+set :model_manager, "doctrine"
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :update_vendors,    true
+
+set :use_composer, true
+set :use_sudo,    false
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :db,         domain, :primary => true
+
+set  :keep_releases,  3
+
+logger.level = Logger::MAX_LEVEL
+
+# Be more verbose by uncommenting the following line
+ logger.level = Logger::MAX_LEVEL
