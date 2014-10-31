c75e15c9f841af39c3320a044bc17061b8259ad1
Stas81 <stas-81@mail.ru>
2014-10-31 19:02:54 +0200

    4success

--- src/Success/EventBundle/Controller/CalendarController.php
+++ src/Success/EventBundle/Controller/CalendarController.php
@@ -8,37 +8,37 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Success\SettingsBundle\SuccessSettingsBundle;
 
 /**
- * @Route("/calendar")
+ * @Route("/calendarevents")
  */
 class CalendarController extends Controller
-{
-    
+{   //pm-di
+   
     /**
-     * @Route("/show", name="show_calendar")
+     * @Route("/{template}/{slug}", name="show_calendar", requirements={"placeholders"=".+"})
      * @Template()
      */
-    public function showAction()
-    {
+    public function showAction(Request $request)
+    { //pm->assingFoundPHTosession($request->query->all())
+        
         return array();
     }
-        
+
     /**
      * @Route("/event/{eventId}", name="show_calendar_event", requirements={"eventId"="\d+"})
      * @Template()
      */
     public function eventAction($eventId){
-
       $event = $this->get('success.event.event_manager')->getEventById($eventId);
       $minutesToVisitEvent = $this->get('success.settings.settings_manager')->getSettingValue('minutesToVisitEvent');
 
       $now = new \DateTime('now');      
-      $allowVisitEvent =  ($event->getStartDateTime()->getTimestamp() - $now->getTimestamp() < $minutesToVisitEvent*60);
       
+      $allowVisitEvent =  ($event->getStartDateTime()->getTimestamp() - $now->getTimestamp() < $minutesToVisitEvent*60);
+      $isPastEvent = $event->getStartDateTime()->getTimestamp() < $now->getTimestamp();
 
         if (!$event){
             throw $this->createNotFoundException('No event found for id='.$eventId);
         }
-
-        return array('event' => $event, 'allowVisitEvent' => $allowVisitEvent);
+        return array('event' => $event, 'allowVisitEvent' => $allowVisitEvent, 'isPastEvent' => $isPastEvent );
     }
 }
--- src/Success/EventBundle/EventListener/CalendarEventListener.php
+++ src/Success/EventBundle/EventListener/CalendarEventListener.php
@@ -50,7 +50,7 @@ class CalendarEventListener
             
             $eventEntity->setAllDay(true); // default is false, set to true if this is an all day event            
             $eventEntity->setFgColor('#FFFFFF'); //set the foreground color of the event's label
-            $eventEntity->setUrl('/calendar/event/'.$cEvent->getId());
+            $eventEntity->setUrl('/calendarevents/event/'.$cEvent->getId());
             //$eventEntity->setCssClass('event-detail');
             $calendarEvent->addEvent($eventEntity);
         }        
