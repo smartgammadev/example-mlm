cc8b54aaa9ac26572f01c95c1ba5082993557cdd
develop1 <whalex76@gmail.com>
2014-11-07 10:05:37 +0200

    Renamed some files only.

--- src/Success/SalesGeneratorBundle/Controller/SalesGeneratorController.php
+++ src/Success/SalesGeneratorBundle/Controller/SalesGeneratorController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class SalesGeneratorController extends Controller
 {
     /**
      * @Route("/sales_generator", name="sales_generator")
-     * @Template("SuccessSalesGeneratorBundle:Default:sales_generator.html.twig")
+     * @Template("SuccessSalesGeneratorBundle:SalesGenerator:sales_generator.html.twig")
      */
     public function salesGeneratorAction()
     {
