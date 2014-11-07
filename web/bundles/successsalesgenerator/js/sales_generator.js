72935a83a09cc9c3892abf056c8d836ddde0813b
develop1 <whalex76@gmail.com>
2014-11-07 18:41:13 +0200

    Added some styling.

--- web/bundles/successsalesgenerator/js/sales_generator.js
+++ web/bundles/successsalesgenerator/js/sales_generator.js
@@ -1,19 +1,11 @@
-$(document).ready(function() {
-//    $('.answer').on('click', function(event) {
-//        panel = $(this);
-//        panel.find('h6').remove();
-//        panel.find('h3').remove();
-//        link = panel.children('.new-question');
-//        console.log(link);
-//        $(this).animate({width:'toggle'},1000, function() {
-//            
-//            panel.remove();
-//            
-//            
-//       }) ;
-//    });
-//    
-//    $('.new-question').on('click', function(event) {
-//        event.preventDefault();    
-//    });
+$(document).ready(function() {    
+    $('.new-question').on('click', function(event) {
+        event.preventDefault();   
+        link = $(this).attr('href');
+        text = $(this).html();
+        $(document).find('.panel').slideUp(400, function() {
+            $(this).remove();
+            window.location.replace(link);
+        });
+    });
 });
