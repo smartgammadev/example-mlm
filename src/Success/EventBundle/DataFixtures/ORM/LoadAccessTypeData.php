6d6d20237f1c79bffaba19b8724851e91d9a7b93
Stas81 <stas-81@mail.ru>
2014-11-10 18:06:18 +0200

    Fixtures comment

--- src/Success/EventBundle/DataFixtures/ORM/LoadAccessTypeData.php
+++ src/Success/EventBundle/DataFixtures/ORM/LoadAccessTypeData.php
@@ -13,14 +13,14 @@ class LoadAccessTypeData implements FixtureInterface {
      */
     public function load(ObjectManager $manager)
     {
-        $newAccessType1 = new EventAccessType();
-        $newAccessType1 -> setName("открытый");
-        $manager->persist($newAccessType1);
-
-        $newAccessType2 = new EventAccessType();        
-        $newAccessType2 -> setName("для партнеров");
-        $manager->persist($newAccessType2);
-        
-        $manager->flush();
+//        $newAccessType1 = new EventAccessType();
+//        $newAccessType1 -> setName("открытый");
+//        $manager->persist($newAccessType1);
+//
+//        $newAccessType2 = new EventAccessType();        
+//        $newAccessType2 -> setName("для партнеров");
+//        $manager->persist($newAccessType2);
+//        
+//        $manager->flush();
     }
 }
